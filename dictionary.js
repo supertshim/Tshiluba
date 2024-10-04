fcc7acc4461a0b0fcf28eb323b0cb02bf00bfbd9
Tshim Tshimanga <149229245+supertshim@users.noreply.github.com>
2024-10-04 13:51:06 -0400

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -65060,7 +65060,7 @@ function maininpt(){
     console.log(firstLetter);
     AlphaLocation = `../alphabetindex/${firstLetter}/${decodeWord}/`;
     console.log(AlphaLocation);
-    /*window.location.href = (AlphaLocation);*/
+    window.location.href = (AlphaLocation);
 }    
 
 
