21d7b89cf201a6f1d7666c23999b1bdf64de5f95
Tshim Tshimanga <149229245+supertshim@users.noreply.github.com>
2024-10-07 11:59:09 -0400

    Add files via upload

--- components/sidebar.js
+++ components/sidebar.js
@@ -9,36 +9,168 @@ class Sidebar extends HTMLElement {
         <h1>Latest Posts</h1>
         </div>
         <div class="latest">
-        <a href="/articles/008-Luba-Grammar-Possessive-Pronouns-And-Adjectives/">
-        <img class="post-icon" src="/articles/008-Luba-Grammar-Possessive-Pronouns-And-Adjectives/nzambi-wanyi.png"><br>
-        <p>Luba Grammar: Possessive Pronouns and Adjectives</p>
-        <a href="/articles/007-Greetings-And-Basic-Introductions-In-Tshiluba/">
-        <img class="post-icon" src="/articles/007-Greetings-And-Basic-Introductions-In-Tshiluba/sign-moyo.png"><br>
-        <p>Sangayi Wabo! Betuabu! Greetings and Basic Introductions in Tshiluba</p>
-        <a href="/articles/006-The-National-Anthem-Of-DRC-Debout-Congolais-In-Tshiluba/">
-        <img class="post-icon" src="/articles/006-The-National-Anthem-Of-DRC-Debout-Congolais-In-Tshiluba/jukayi.png"><br>
-        <p>The National Anthem 'Debout Congolais' in Tshiluba</p>
-        <a href="/articles/005-To-Be-Or-Not-To-Be/">
-        <img class="post-icon" src="/articles/005-To-Be-Or-Not-To-Be/to-be.png"><br>
-        <p>To Be or Not To Be: Conjugating 'To Be' in Tshiluba</p>
-        <a href="/articles/004-How-To-Count-In-Tshiluba/">
-        <img class="post-icon" src="/articles/004-How-To-Count-In-Tshiluba/count2.png"><br>
-        <p>How to count in Tshiluba</p>
-        </a><br>
-        <a href="/articles/003-Exploring-The-Noun-Class-System-In-Tshiluba/">
-        <img class="post-icon" src="/articles/003-Exploring-The-Noun-Class-System-In-Tshiluba/Luba-Grammaire.png"><br>
-        <p>Exploring the Noun Class System in Tshiluba: A Detailed Examination</p>
-        </a><br>
 
-        <a href="/articles/002-The-Mu-Ba-Tshiluba-Noun-Class/">
-        <img class="post-icon" src="/articles/002-The-Mu-Ba-Tshiluba-Noun-Class/Mu-ba.png"><br>
-        <p>The Mu-Ba Tshiluba Noun Class</p>
-        </a><br>
+<!-- Article 8 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/008-Luba-Grammar-Possessive-Pronouns-And-Adjectives/">
+                        <img class="post-icon" src="/articles/008-Luba-Grammar-Possessive-Pronouns-And-Adjectives/nzambi-wanyi.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/008-Luba-Grammar-Possessive-Pronouns-And-Adjectives/">
+                            Luba Grammar: Possessive Pronouns and Adjectives
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 7 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/007-Greetings-And-Basic-Introductions-In-Tshiluba/">
+                        <img class="post-icon" src="/articles/007-Greetings-And-Basic-Introductions-In-Tshiluba/sign-moyo.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/007-Greetings-And-Basic-Introductions-In-Tshiluba/">
+                            Sangayi Wabo! Betuabu! Greetings and Basic Introductions in Tshiluba
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 6 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/006-The-National-Anthem-Of-DRC-Debout-Congolais-In-Tshiluba/">
+                        <img class="post-icon" src="/articles/006-The-National-Anthem-Of-DRC-Debout-Congolais-In-Tshiluba/jukayi.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/music/">
+                            MUSIC
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/006-The-National-Anthem-Of-DRC-Debout-Congolais-In-Tshiluba/">
+                            The National Anthem 'Debout Congolais' in Tshiluba
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 5 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/005-To-Be-Or-Not-To-Be/">
+                        <img class="post-icon" src="/articles/005-To-Be-Or-Not-To-Be/to-be.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/005-To-Be-Or-Not-To-Be/">
+                            To Be or Not To Be: Conjugating 'To Be' in Tshiluba
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 4 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/004-How-To-Count-In-Tshiluba/">
+                        <img class="post-icon" src="/articles/004-How-To-Count-In-Tshiluba/count2.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/numbers/">
+                            NUMBERS
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/004-How-To-Count-In-Tshiluba/">
+                            How to count in Tshiluba
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 3 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/003-Exploring-The-Noun-Class-System-In-Tshiluba/">
+                        <img class="post-icon" src="/articles/003-Exploring-The-Noun-Class-System-In-Tshiluba/Luba-Grammaire.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/003-Exploring-The-Noun-Class-System-In-Tshiluba/">
+                            Exploring the Noun Class System in Tshiluba: A Detailed Examination
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 2 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/002-The-Mu-Ba-Tshiluba-Noun-Class/">
+                        <img class="post-icon" src="/articles/002-The-Mu-Ba-Tshiluba-Noun-Class/Mu-ba.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/002-The-Mu-Ba-Tshiluba-Noun-Class/">
+                            The Mu-Ba Tshiluba Noun Class
+                        </a>
+                    </div>
+                </div>
+            </div>
+<!-- Article 1 -->
+            <div class="mini-article">
+                <div class="mini-img">
+                    <a style="text-decoration:none;" href="/articles/001-The-ka-tu-tshiluba-noun-class/">
+                        <img class="post-icon" src="/articles/001-The-ka-tu-tshiluba-noun-class/katu.png">
+                    </a>
+                </div>
+                <div class="mini-article-title">
+                    <div class="sidebar-blog-category">
+                        <a href="/blog/grammar/">
+                            GRAMMAR
+                        </a>
+                    </div>
+                    <div class="mini-article-title-text">
+                        <a style="text-decoration:none;" href="/articles/001-The-ka-tu-tshiluba-noun-class/">
+                            The Ka-Tu Tshiluba Noun Class
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-        <a href="/articles/001-The-ka-tu-tshiluba-noun-class/">
-        <img class="post-icon" src="/articles/001-The-ka-tu-tshiluba-noun-class/katu.png"><br>
-        <p>The Ka-Tu Tshiluba Noun Class</p>
-        </a><br>
         </div>
         
         <div class="pageLinks">
