b37e98e32925593a9d1c819d885b22f813e0d705
Tshim Tshimanga <149229245+supertshim@users.noreply.github.com>
2024-10-07 14:08:41 -0400

    Add files via upload

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,6 +2,20 @@ class Sidebar extends HTMLElement {
     constructor() {
         super()
         this.innerHTML = `
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3865469888003630"
+     crossorigin="anonymous"></script>
+<!-- Square SideBar Ad -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3865469888003630"
+     data-ad-slot="2206397567"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
         <span>Word of the Day</span>
         
         <muakuwd-card class="MuakuWD"></muakuwd-card>
